dd8698c373dbd6ab19e45ca77412c2a422809e1b
Volodymyr1Korol <volodymyrkorol18@gmail.com>
2022-11-10 21:34:22 +0200

    7 hw need check.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function createGallery(images) {
+    return images
+        .map(({ original, preview, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div>`;
+        })
+        .join('');
+}
+
+const imageMarkup = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createLightBox(img) {
+    const instance = basicLightbox.create(`<img src="${img.dataset.source}">`, {
+        onShow: instance => {
+            document.addEventListener('keydown', closeFromKeyboard.bind(instance));
+        },
+        onClose: instance => {
+            document.removeEventListener('keydown', closeFromKeyboard.bind(instance));
+        },
+    });
+
+    instance.show();
+}
+
+function openGalleryModal(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const imgOnClick = e.target;
+
+    createLightBox(imgOnClick);
+}
+
+function closeFromKeyboard(e) {
+    if (e.code === 'Escape') {
+        this.close();
+    }
+}
+
+galleryEl.addEventListener('click', openGalleryModal);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('ul.gallery');
+
+function createGallery(images) {
+    return images.map(({ original, preview, description }) => {
+        return `
+        <div class="gallery">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+        </div>`
+    }).join('');
+}
+
+const imageMarkup = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterbegin', imageMarkup);
+
+const lightbox = new SimpleLightbox('div.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
